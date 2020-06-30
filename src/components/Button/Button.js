2849b2f0f032763715dd0b2134b2a1f62509a24c
Khusharth <khusharth19@gmail.com>
2020-06-30 11:24:35 +0530

    Changed button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./Button.scss";
 
-const Button = ({ children, onClick, marginTop }) => {
+const Button = ({ children, onClick, marginTop, disabled, type }) => {
     return (
         <button
             style={{ marginTop: `${marginTop}` }}
             onClick={onClick}
-            className='btn--primary'>
+            className='btn--primary'
+            disabled={disabled}
+            type={type}
+        >
             {children}
         </button>
     );
+};
+
+Button.defaultProps = {
+    disabled: false,
+    type: 'button'
 };
 
 export default Button;
